d88da8047c76c464f67d2042c5cf809d993a428e
Olga Andrianova <andrianova@majordomo.ru>
2020-03-13 18:35:32 +0300

    change_rabbit_routing_key

--- src/main/java/ru/majordomo/hms/rc/staff/common/Constants.java
+++ src/main/java/ru/majordomo/hms/rc/staff/common/Constants.java
@@ -3,4 +3,8 @@ package ru.majordomo.hms.rc.staff.common;
 public class Constants {
     public static final String PM = "pm";
     public static final String TE = "te";
+    /**
+     * rabbit routing key. Так как не совпадает с именем приложения
+     */
+    public static final String RC_STAFF_ROUTING_KEY = "rc.staff";
 }
